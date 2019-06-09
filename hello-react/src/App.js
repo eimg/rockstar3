4ed37ad399f715c73a83c8d86c9e3c869891e69b
Ei Maung <eimg@fairwayweb.com>
2019-06-09 15:50:40 +0630

    updated hello-react

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import List from "./List";
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: ['Bob', 'Alice', 'Tom']
+        };
+
+        this.input = React.createRef();
+        this.add = this.add.bind(this);
+    }
+
+    add() {
+        this.setState({
+            data: [...this.state.data, this.input.current.value],
+        });
+    }
+
     render() {
         return (
-            <div>Hello React</div>
+            <div>
+                <List data={this.state.data} />
+                <input type="text" ref={this.input} />
+                <button onClick={this.add}>+</button>
+            </div>
         );
     }
 }
